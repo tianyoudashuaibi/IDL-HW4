76c3426f9c8a117ba1dda0251586d0ee3a38b0bf
CPF <50857406+tianyoudashuaibi@users.noreply.github.com>
2025-04-09 01:08:31 -0700

    Update linear.py

--- mytorch/nn/linear.py
+++ mytorch/nn/linear.py
@@ -2,50 +2,32 @@ import numpy as np
 
 class Linear:
     def __init__(self, in_features, out_features):
-        """
-        Initialize the weights and biases with zeros
-        W shape: (out_features, in_features)
-        b shape: (out_features,)  # Changed from (out_features, 1) to match PyTorch
-        """
-        # DO NOT MODIFY
         self.W = np.zeros((out_features, in_features))
         self.b = np.zeros(out_features)
 
-
     def init_weights(self, W, b):
-        """
-        Initialize the weights and biases with the given values.
-        """
-        # DO NOT MODIFY
         self.W = W
         self.b = b
 
     def forward(self, A):
-        """
-        :param A: Input to the linear layer with shape (*, in_features)
-        :return: Output Z with shape (*, out_features)
-        
-        Handles arbitrary batch dimensions like PyTorch
-        """
-        # TODO: Implement forward pass
-        
-        # Store input for backward pass
+        self.input_shape = A.shape
+        batch_size = np.prod(A.shape[:-1])
+        in_features = A.shape[-1]
+        A_2d = A.reshape(batch_size, in_features)
+        Z_2d = A_2d @ self.W.T + self.b
+        out_features = self.W.shape[0]
+        Z = Z_2d.reshape(*self.input_shape[:-1], out_features)
         self.A = A
-        
-        raise NotImplementedError
+        return Z
 
     def backward(self, dLdZ):
-        """
-        :param dLdZ: Gradient of loss wrt output Z (*, out_features)
-        :return: Gradient of loss wrt input A (*, in_features)
-        """
-        # TODO: Implement backward pass
-
-        # Compute gradients (refer to the equations in the writeup)
-        self.dLdA = NotImplementedError
-        self.dLdW = NotImplementedError
-        self.dLdb = NotImplementedError
-        self.dLdA = NotImplementedError
-        
-        # Return gradient of loss wrt input
-        raise NotImplementedError
+        out_features = self.W.shape[0]
+        batch_size = np.prod(dLdZ.shape[:-1])
+        dLdZ_2d = dLdZ.reshape(batch_size, out_features)
+        in_features = self.W.shape[1]
+        A_2d = self.A.reshape(batch_size, in_features)
+        self.dLdb = np.sum(dLdZ_2d, axis=0)  
+        self.dLdW = dLdZ_2d.T @ A_2d
+        dLdA_2d = dLdZ_2d @ self.W
+        dLdA = dLdA_2d.reshape(*self.input_shape)
+        return dLdA
